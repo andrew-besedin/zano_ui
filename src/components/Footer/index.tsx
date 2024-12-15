ae3aea75a933791723b73111cb157624e3867cb6
jejolare <jejolare@gmail.com>
2024-12-16 02:20:53 +0700

    remove deps & update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from 'react';
 import { classes } from '../../utils';
-import styles from "./styles.module.scss"; 
+import styles from "./styles.module.scss";
 import Link from 'next/link';
 
-type SelectedLink = 
+type SelectedLink =
     "home" |
     "explorer" |
     "trade" |
@@ -16,42 +17,50 @@ const links: {
     link: string;
     disabled?: boolean;
 }[] = [
-    {
-        title: "Zano Website",
-        type: "home",
-        link: "https://zano.org/"
-    },
-    {
-        title: "Explorer",
-        type: "explorer",
-        link: "https://explorer.zano.org/"
-    },
-    {
-        title: "Trade",
-        type: "trade",
-        link: "https://trade.zano.org/"
-    },
-    {
-        title: "Auction",
-        type: "auction",
-        link: "https://auction.zano.org/"
-    },
-    {
-        title: "Messenger",
-        type: "messenger",
-        link: "https://messenger.zano.org/"
-    },
-    {
-        title: "Wrapped Zano",
-        type: "wrapped-zano",
-        link: "https://wrapped.zano.org/"
-    }
-];
-
-
-const selectedLink: SelectedLink = "messenger";
+        {
+            title: "Zano Website",
+            type: "home",
+            link: "https://zano.org"
+        },
+        {
+            title: "Explorer",
+            type: "explorer",
+            link: "https://explorer.zano.org"
+        },
+        {
+            title: "Trade",
+            type: "trade",
+            link: "https://trade.zano.org"
+        },
+        {
+            title: "Auction",
+            type: "auction",
+            link: "https://auction.zano.org"
+        },
+        {
+            title: "Messenger",
+            type: "messenger",
+            link: "https://messenger.zano.org"
+        },
+        {
+            title: "Wrapped Zano",
+            type: "wrapped-zano",
+            link: "https://wrapped.zano.org"
+        }
+    ];
 
 function Footer() {
+
+
+    const [selectedLink, setRecognizedLink] = useState<SelectedLink | null>(null);
+
+    useEffect(() => {
+        const currentDomain = window.location.hostname;
+        const match = links.find(link => link.link.includes(currentDomain));
+        setRecognizedLink(match ? match.type : null);
+    }, []);
+
+    
     return (
         <footer className={styles.footer}>
             <div className={styles.footer__refs}>
@@ -62,7 +71,7 @@ function Footer() {
                                 (e.type === selectedLink) && styles.footer__ref_selected,
                                 e.disabled && styles.footer__ref_disabled
                             )
-                        } 
+                        }
                         key={e.type}
                         href={e.link}
                         rel="noopener noreferrer"
