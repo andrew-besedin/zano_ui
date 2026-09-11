7ce30352d643168f6ea8d70cc633a588d9fe0739
Andrew Besedin <andrewprog97@gmail.com>
2026-09-11 14:56:00 +0300

    fix: remove 'next/image' module global type declaration due to name collision with 'next/image' from 'next' package

--- global.d.ts
+++ global.d.ts
@@ -15,26 +15,3 @@ declare module "*.svg?url" {
     const src: string;
     export default src;
 }
-
-declare module "next/image" {
-    import * as React from "react";
-
-    export interface ImageProps extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'src' | 'width' | 'height'> {
-        src: string | StaticImageData;
-        alt: string;
-        width?: number | `${number}`;
-        height?: number | `${number}`;
-        fill?: boolean;
-        sizes?: string;
-        quality?: number;
-        priority?: boolean;
-        loading?: 'eager' | 'lazy';
-        placeholder?: 'blur' | 'empty';
-        blurDataURL?: string;
-        unoptimized?: boolean;
-    }
-
-    const Image: React.FC<ImageProps>;
-
-    export default Image;
-}
